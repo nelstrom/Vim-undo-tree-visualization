6f29823a88d4303a957b36eba32180e4817010e8
Drew Neil <andrew.jr.neil@gmail.com>
2011-01-23 23:18:55 +0200

    Lots of setup. Not much fruit.

--- public/vim-tree-viz.js
+++ public/vim-tree-viz.js
@@ -1,7 +1,129 @@
-window.onload = function() {  
-  
-  var paper = Raphael(document.getElementById("notepad"), 320, 200);
-  var circle = paper.circle(50, 40, 10);
-  circle.attr("fill", "#f00");
-  circle.attr("stroke", "#fff");
-}  
+var coords = {}
+window.onload = function() {
+  var canvasWidth = 640,
+      canvasHeight = 480,
+      canvasMargin = 90
+      noOfStates = 6,
+      availableHeight = canvasHeight - (canvasMargin * 2),
+      availableWidth  = canvasWidth  - (canvasMargin * 2),
+      lineLength = (availableWidth / (noOfStates-1));
+
+  coords = {
+    s1: {x: 0, y: 0},
+    s2: {x: 0, y: 0},
+    s3: {x: 0, y: 0},
+    s4: {x: 0, y: 0},
+    s5: {x: 0, y: 0},
+    s6: {x: 0, y: 0},
+
+    t3: {x: 0, y: 0},
+    t4: {x: 0, y: 0},
+    t5: {x: 0, y: 0},
+    t6: {x: 0, y: 0},
+
+    b3: {x: 0, y: 0},
+    b4: {x: 0, y: 0}
+  };
+
+
+  coords.s1.x = canvasMargin;
+  coords.s1.y = canvasMargin + (availableHeight/2);
+
+  coords.s2.x = coords.s1.x + (lineLength*1);
+  coords.s3.x = coords.s1.x + (lineLength*2);
+  coords.s4.x = coords.s1.x + (lineLength*3);
+  coords.s5.x = coords.s1.x + (lineLength*4);
+  coords.s6.x = coords.s1.x + (lineLength*5);
+
+  coords.s2.y = coords.s1.y;
+  coords.s3.y = coords.s1.y;
+  coords.s4.y = coords.s1.y;
+  coords.s5.y = coords.s1.y;
+  coords.s6.y = coords.s1.y;
+
+  coords.t3.x = coords.s2.x - (lineLength * Math.sin(60));
+  coords.t3.y = coords.s2.y + (lineLength * Math.cos(60));
+  coords.t4.x = coords.t3.x + (lineLength*1);
+  coords.t4.y = coords.t3.y;
+  coords.t5.x = coords.t4.x + (lineLength*1);
+  coords.t5.y = coords.t4.y;
+  coords.t6.x = coords.t5.x + (lineLength*1);
+  coords.t6.y = coords.t5.y;
+
+  coords.b3.x = coords.s2.x - (lineLength * Math.sin(60));
+  coords.b3.y = coords.s2.y - (lineLength * Math.cos(60));
+  coords.b4.x = coords.b3.x + (lineLength*1);
+  coords.b4.y = coords.b3.y;
+
+  var chronologicalCoords = [
+    {x: coords.s1.x, y: coords.s1.y},
+    {x: coords.s2.x, y: coords.s2.y},
+    {x: coords.s3.x, y: coords.s3.y},
+    {x: coords.s4.x, y: coords.s4.y},
+    {x: coords.s5.x, y: coords.s5.y},
+    {x: coords.s6.x, y: coords.s6.y}
+  ];
+
+  var topBranchCoords = [
+    {x: coords.s1.x, y: coords.s1.y},
+    {x: coords.s2.x, y: coords.s2.y},
+    {x: coords.t3.x, y: coords.t3.y},
+    {x: coords.t4.x, y: coords.t4.y},
+    {x: coords.t5.x, y: coords.t5.y},
+    {x: coords.t6.x, y: coords.t6.y}
+  ];
+
+  var bottomBranchCoords = [
+    {x: coords.s1.x, y: coords.s1.y},
+    {x: coords.s2.x, y: coords.s2.y},
+    {x: coords.b3.x, y: coords.b3.y},
+    {x: coords.b4.x, y: coords.b4.y}
+  ];
+
+  var topFork = {
+    values: ['circle', 'circle', 'circle', 'circle'],
+    coords: topBranchCoords
+  };
+  var bottomFork = {
+    values: ['circle', 'circle', 'circle', 'circle'],
+    coords: bottomBranchCoords
+  };
+  var topMix = {
+    values: ['circle','circle','point','point','circle','circle'],
+    coords: topBranchCoords
+  };
+  var bottomMix = {
+    values: ['circle', 'circle', 'circle', 'circle'],
+    coords: bottomBranchCoords
+  };
+  var chronological = {
+    values: ['circle', 'circle', 'circle', 'circle', 'circle', 'circle'],
+    coords: chronologicalCoords
+  };
+
+  var paper = Raphael(document.getElementById("notepad"), canvasWidth, canvasHeight);
+  var radius = 10,
+      lineHeight = 4;
+
+  outline = topMix;
+  coordList = outline.coords;
+
+  for (var i = 0; i < coordList.length; i++) {
+    var coords = coordList[i];
+
+    if (i !== coordList.length-1) {
+      var next = coordList[i+1];
+      var pathString = "M" + coords.x + " " + coords.y + "L" + next.x + " " + next.y
+
+      var c = paper.path(pathString);
+      c.attr("stroke", "#008");
+      c.attr("stroke-width", "5");
+    }
+    if (outline.values[i] === 'circle') {
+      var circle = paper.circle(coords.x, coords.y, radius);
+      circle.attr("fill", "#fff");
+      circle.attr("stroke", "#000");
+    }
+
+  };
+}
