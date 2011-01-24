9a528e46e8c5498c9639eb23e53ace76bfcfaf5e
Drew Neil <andrew.jr.neil@gmail.com>
2011-01-24 18:29:24 +0200

    Tidying up the setup, and logging coords.

--- public/treegraph.js
+++ public/treegraph.js
@@ -1,5 +1,5 @@
 (function() {
-  var availableHeight, availableWidth, coords, forkAngle, lineLength, margin, nodeCount, setup, totalHeight, totalWidth;
+  var availableHeight, availableWidth, coords, forkAngle, lineLength, margin, nodeCount, point, totalHeight, totalWidth, _i, _len, _ref;
   totalWidth = 640;
   totalHeight = 480;
   margin = 90;
@@ -9,58 +9,59 @@
   lineLength = availableWidth / (nodeCount - 1);
   forkAngle = Math.PI / 3;
   coords = {};
-  setup = function() {
-    coords.s1 = {
-      x: margin,
-      y: totalHeight / 2
-    };
-    coords.s2 = {
-      x: margin + lineLength * 1,
-      y: totalHeight / 2
-    };
-    coords.s3 = {
-      x: margin + lineLength * 2,
-      y: totalHeight / 2
-    };
-    coords.s4 = {
-      x: margin + lineLength * 3,
-      y: totalHeight / 2
-    };
-    coords.s5 = {
-      x: margin + lineLength * 4,
-      y: totalHeight / 2
-    };
-    coords.s6 = {
-      x: margin + lineLength * 5,
-      y: totalHeight / 2
-    };
-    coords.b3 = {
-      x: coords.s2.x + (lineLength * Math.cos(forkAngle)),
-      y: coords.s2.y + (lineLength * Math.sin(forkAngle))
-    };
-    coords.b4 = {
-      x: coords.b3.x + lineLength * 1,
-      y: coords.b3.y
-    };
-    coords.t3 = {
-      x: coords.s2.x + (lineLength * Math.cos(forkAngle)),
-      y: coords.s2.y - (lineLength * Math.sin(forkAngle))
-    };
-    coords.t4 = {
-      x: coords.t3.x + lineLength * 1,
-      y: coords.t3.y
-    };
-    coords.t5 = {
-      x: coords.t3.x + lineLength * 2,
-      y: coords.t3.y
-    };
-    coords.t6 = {
-      x: coords.t3.x + lineLength * 3,
-      y: coords.t3.y
-    };
-    return coords;
+  coords.s1 = {
+    x: margin,
+    y: totalHeight / 2
   };
-  setup();
-  console.log("t4.x: " + coords.t4.x);
-  console.log("t4.y: " + coords.t4.y);
+  coords.s2 = {
+    x: margin + lineLength * 1,
+    y: totalHeight / 2
+  };
+  coords.s3 = {
+    x: margin + lineLength * 2,
+    y: totalHeight / 2
+  };
+  coords.s4 = {
+    x: margin + lineLength * 3,
+    y: totalHeight / 2
+  };
+  coords.s5 = {
+    x: margin + lineLength * 4,
+    y: totalHeight / 2
+  };
+  coords.s6 = {
+    x: margin + lineLength * 5,
+    y: totalHeight / 2
+  };
+  coords.b3 = {
+    x: coords.s2.x + (lineLength * Math.cos(forkAngle)),
+    y: coords.s2.y + (lineLength * Math.sin(forkAngle))
+  };
+  coords.b4 = {
+    x: coords.b3.x + lineLength * 1,
+    y: coords.b3.y
+  };
+  coords.t3 = {
+    x: coords.s2.x + (lineLength * Math.cos(forkAngle)),
+    y: coords.s2.y - (lineLength * Math.sin(forkAngle))
+  };
+  coords.t4 = {
+    x: coords.t3.x + lineLength * 1,
+    y: coords.t3.y
+  };
+  coords.t5 = {
+    x: coords.t3.x + lineLength * 2,
+    y: coords.t3.y
+  };
+  coords.t6 = {
+    x: coords.t3.x + lineLength * 3,
+    y: coords.t3.y
+  };
+  _ref = ['s1', 's2', 's3', 's4', 's5', 's6', 't3', 't4', 't5', 't6', 'b3', 'b4'];
+  for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+    point = _ref[_i];
+    console.log(point);
+    console.log("   x: " + coords[point].x);
+    console.log("   y: " + coords[point].y);
+  }
 }).call(this);
