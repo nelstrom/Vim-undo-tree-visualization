ab24de6bf6f05eeaf6dd7b9cd099041090d64747
Drew Neil <andrew.jr.neil@gmail.com>
2011-02-14 23:19:23 +0200

    Complete the story.

--- public/story.js
+++ public/story.js
@@ -99,4 +99,629 @@
       }
     ]
   });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,s3,s4',
+      active: true
+    },
+    timelineRevised: {
+      points: 's1,s2'
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2'
+      }, {
+        position: 's3',
+        active: true
+      }, {
+        position: 's4'
+      }, {
+        position: 's3'
+      }, {
+        position: 's3'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,s3,s4',
+      active: true
+    },
+    timelineRevised: {
+      points: 's1,s2'
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2',
+        active: true
+      }, {
+        position: 's3'
+      }, {
+        position: 's4'
+      }, {
+        position: 's2'
+      }, {
+        position: 's2'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,b3,b4'
+    },
+    timelineRevised: {
+      points: 's1,s2,t3',
+      active: true
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2'
+      }, {
+        position: 'b3'
+      }, {
+        position: 'b4'
+      }, {
+        position: 't3',
+        active: true
+      }, {
+        position: 't3'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,b3,b4'
+    },
+    timelineRevised: {
+      points: 's1,s2,t3,t4',
+      active: true
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2'
+      }, {
+        position: 'b3'
+      }, {
+        position: 'b4'
+      }, {
+        position: 't3'
+      }, {
+        position: 't4',
+        active: true
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,b3,b4'
+    },
+    timelineRevised: {
+      points: 's1,s2,t3,t4',
+      active: true
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2'
+      }, {
+        position: 'b3'
+      }, {
+        position: 'b4'
+      }, {
+        position: 't3',
+        active: true
+      }, {
+        position: 't4'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,b3,b4'
+    },
+    timelineRevised: {
+      points: 's1,s2,t3,t4',
+      active: true
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2',
+        active: true
+      }, {
+        position: 'b3'
+      }, {
+        position: 'b4'
+      }, {
+        position: 't3'
+      }, {
+        position: 't4'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,b3,b4'
+    },
+    timelineRevised: {
+      points: 's1,s2,t3,t4',
+      active: true
+    },
+    nodes: [
+      {
+        position: 's1',
+        active: true
+      }, {
+        position: 's2'
+      }, {
+        position: 'b3'
+      }, {
+        position: 'b4'
+      }, {
+        position: 't3'
+      }, {
+        position: 't4'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,b3,b4'
+    },
+    timelineRevised: {
+      points: 's1,s2,t3,t4',
+      active: true
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2',
+        active: true
+      }, {
+        position: 'b3'
+      }, {
+        position: 'b4'
+      }, {
+        position: 't3'
+      }, {
+        position: 't4'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,b3,b4'
+    },
+    timelineRevised: {
+      points: 's1,s2,t3,t4',
+      active: true
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2'
+      }, {
+        position: 'b3'
+      }, {
+        position: 'b4'
+      }, {
+        position: 't3',
+        active: true
+      }, {
+        position: 't4'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,b3,b4'
+    },
+    timelineRevised: {
+      points: 's1,s2,t3,t4',
+      active: true
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2'
+      }, {
+        position: 'b3'
+      }, {
+        position: 'b4'
+      }, {
+        position: 't3'
+      }, {
+        position: 't4',
+        active: true
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,s3,s4'
+    },
+    timelineRevised: {
+      points: 's1,s2,s5,s6',
+      active: true
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2'
+      }, {
+        position: 's3'
+      }, {
+        position: 's4'
+      }, {
+        position: 's5'
+      }, {
+        position: 's6',
+        active: true
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,s3,s4'
+    },
+    timelineRevised: {
+      points: 's1,s2,s5,s6',
+      active: true
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2'
+      }, {
+        position: 's3'
+      }, {
+        position: 's4'
+      }, {
+        position: 's5',
+        active: true
+      }, {
+        position: 's6'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,s3,s4'
+    },
+    timelineRevised: {
+      points: 's1,s2,s5,s6',
+      active: true
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2'
+      }, {
+        position: 's3'
+      }, {
+        position: 's4',
+        active: true
+      }, {
+        position: 's5'
+      }, {
+        position: 's6'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,s3,s4'
+    },
+    timelineRevised: {
+      points: 's1,s2,s5,s6',
+      active: true
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2'
+      }, {
+        position: 's3',
+        active: true
+      }, {
+        position: 's4'
+      }, {
+        position: 's5'
+      }, {
+        position: 's6'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,s3,s4'
+    },
+    timelineRevised: {
+      points: 's1,s2,s5,s6',
+      active: true
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2',
+        active: true
+      }, {
+        position: 's3'
+      }, {
+        position: 's4'
+      }, {
+        position: 's5'
+      }, {
+        position: 's6'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,s3,s4'
+    },
+    timelineRevised: {
+      points: 's1,s2,s5,s6',
+      active: true
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2'
+      }, {
+        position: 's3',
+        active: true
+      }, {
+        position: 's4'
+      }, {
+        position: 's5'
+      }, {
+        position: 's6'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,s3,s4'
+    },
+    timelineRevised: {
+      points: 's1,s2,s5,s6',
+      active: true
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2'
+      }, {
+        position: 's3'
+      }, {
+        position: 's4',
+        active: true
+      }, {
+        position: 's5'
+      }, {
+        position: 's6'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,b3,b4',
+      active: true
+    },
+    timelineRevised: {
+      points: 's1,s2,t3,t5,t6'
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2'
+      }, {
+        position: 'b3'
+      }, {
+        position: 'b4',
+        active: true
+      }, {
+        position: 't5'
+      }, {
+        position: 't6'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,b3,b4'
+    },
+    timelineRevised: {
+      points: 's1,s2,t3,t5,t6',
+      active: true
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2'
+      }, {
+        position: 'b3'
+      }, {
+        position: 'b4'
+      }, {
+        position: 't5',
+        active: true
+      }, {
+        position: 't6'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,b3,b4'
+    },
+    timelineRevised: {
+      points: 's1,s2,t3,t5,t6',
+      active: true
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2'
+      }, {
+        position: 'b3'
+      }, {
+        position: 'b4'
+      }, {
+        position: 't5'
+      }, {
+        position: 't6',
+        active: true
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,b3,b4'
+    },
+    timelineRevised: {
+      points: 's1,s2,t3,t5,t6',
+      active: true
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2'
+      }, {
+        position: 'b3'
+      }, {
+        position: 'b4'
+      }, {
+        position: 't5',
+        active: true
+      }, {
+        position: 't6'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,b3,b4',
+      active: true
+    },
+    timelineRevised: {
+      points: 's1,s2,t3,t5,t6'
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2'
+      }, {
+        position: 'b3'
+      }, {
+        position: 'b4',
+        active: true
+      }, {
+        position: 't5'
+      }, {
+        position: 't6'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,b3,b4',
+      active: true
+    },
+    timelineRevised: {
+      points: 's1,s2,t3,t5,t6'
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2'
+      }, {
+        position: 'b3',
+        active: true
+      }, {
+        position: 'b4'
+      }, {
+        position: 't5'
+      }, {
+        position: 't6'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,b3,b4',
+      active: true
+    },
+    timelineRevised: {
+      points: 's1,s2,t3,t5,t6'
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2',
+        active: true
+      }, {
+        position: 'b3'
+      }, {
+        position: 'b4'
+      }, {
+        position: 't5'
+      }, {
+        position: 't6'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,b3,b4',
+      active: true
+    },
+    timelineRevised: {
+      points: 's1,s2,t3,t5,t6'
+    },
+    nodes: [
+      {
+        position: 's1',
+        active: true
+      }, {
+        position: 's2'
+      }, {
+        position: 'b3'
+      }, {
+        position: 'b4'
+      }, {
+        position: 't5'
+      }, {
+        position: 't6'
+      }
+    ]
+  });
 }).call(this);
