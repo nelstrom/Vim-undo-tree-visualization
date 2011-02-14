1c1272bcdcc4ece4f880c27be72cb7ec57b1f500
Drew Neil <andrew.jr.neil@gmail.com>
2011-02-14 22:07:13 +0200

    Add DocumentState.js and story.js to coffeescript.html.

---
+++ public/story.coffee
@@ -0,0 +1,15 @@
+new DocumentState
+  timelineOriginal:
+    points: 's1'
+    active: true
+  timelineRevised:
+    points: 's1'
+  nodes: [
+    { position: 's1', active: true }
+    { position: 's1' }
+    { position: 's1' }
+    { position: 's1' }
+    { position: 's1' }
+    { position: 's1' }
+  ]
+
---
+++ public/story.js
@@ -0,0 +1,27 @@
+(function() {
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1',
+      active: true
+    },
+    timelineRevised: {
+      points: 's1'
+    },
+    nodes: [
+      {
+        position: 's1',
+        active: true
+      }, {
+        position: 's1'
+      }, {
+        position: 's1'
+      }, {
+        position: 's1'
+      }, {
+        position: 's1'
+      }, {
+        position: 's1'
+      }
+    ]
+  });
+}).call(this);
