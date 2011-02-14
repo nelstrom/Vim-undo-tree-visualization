2f88f18b42d29db4a71cc90357580531a08a1eab
Drew Neil <andrew.jr.neil@gmail.com>
2011-02-14 22:09:40 +0200

    Build first 4 states of story.

--- public/story.js
+++ public/story.js
@@ -24,4 +24,79 @@
       }
     ]
   });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2',
+      active: true
+    },
+    timelineRevised: {
+      points: 's1,s2'
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2',
+        active: true
+      }, {
+        position: 's2'
+      }, {
+        position: 's2'
+      }, {
+        position: 's2'
+      }, {
+        position: 's2'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,s3',
+      active: true
+    },
+    timelineRevised: {
+      points: 's1,s2,s3'
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2'
+      }, {
+        position: 's3',
+        active: true
+      }, {
+        position: 's3'
+      }, {
+        position: 's3'
+      }, {
+        position: 's3'
+      }
+    ]
+  });
+  new DocumentState({
+    timelineOriginal: {
+      points: 's1,s2,s3,s4',
+      active: true
+    },
+    timelineRevised: {
+      points: 's1,s2,s3,s4'
+    },
+    nodes: [
+      {
+        position: 's1'
+      }, {
+        position: 's2'
+      }, {
+        position: 's3'
+      }, {
+        position: 's4',
+        active: true
+      }, {
+        position: 's4'
+      }, {
+        position: 's4'
+      }
+    ]
+  });
 }).call(this);
