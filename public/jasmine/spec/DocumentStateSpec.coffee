d0f0a877e1dc867f3dde6672790eb161ba9abb31
Drew Neil <andrew.jr.neil@gmail.com>
2011-02-14 19:39:59 +0200

    Spec for activeTrack.

--- public/jasmine/spec/DocumentStateSpec.coffee
+++ public/jasmine/spec/DocumentStateSpec.coffee
@@ -6,9 +6,13 @@ describe "DocumentState", ->
       points: 's1,s2,s5,s6'
 
   describe "timelineOriginal", ->
-  it "returns a list", ->
-    expect(state.timelineOriginal).toEqual ['s1','s2','s3','s4']
+    it "returns a list", ->
+      expect(state.timelineOriginal).toEqual ['s1','s2','s3','s4']
 
   describe "timelineRevised", ->
-  it "returns a list", ->
-    expect(state.timelineRevised).toEqual ['s1','s2','s5','s6']
+    it "returns a list", ->
+      expect(state.timelineRevised).toEqual ['s1','s2','s5','s6']
+
+  describe "activeTrack", ->
+    it "returns 'timelineRevised'", ->
+      expect(state.activeTrack).toEqual 'timelineRevised'
--- public/jasmine/spec/DocumentStateSpec.js
+++ public/jasmine/spec/DocumentStateSpec.js
@@ -9,13 +9,20 @@
         points: 's1,s2,s5,s6'
       }
     });
-    describe("timelineOriginal", function() {});
-    it("returns a list", function() {
-      return expect(state.timelineOriginal).toEqual(['s1', 's2', 's3', 's4']);
+    describe("timelineOriginal", function() {
+      return it("returns a list", function() {
+        return expect(state.timelineOriginal).toEqual(['s1', 's2', 's3', 's4']);
+      });
     });
-    describe("timelineRevised", function() {});
-    return it("returns a list", function() {
-      return expect(state.timelineRevised).toEqual(['s1', 's2', 's5', 's6']);
+    describe("timelineRevised", function() {
+      return it("returns a list", function() {
+        return expect(state.timelineRevised).toEqual(['s1', 's2', 's5', 's6']);
+      });
+    });
+    return describe("activeTrack", function() {
+      return it("returns 'timelineRevised'", function() {
+        return expect(state.activeTrack).toEqual('timelineRevised');
+      });
     });
   });
 }).call(this);
