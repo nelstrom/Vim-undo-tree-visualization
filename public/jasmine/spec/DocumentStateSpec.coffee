c30f9cf7b2f51ba85818401751ee483b696bbe3c
Drew Neil <andrew.jr.neil@gmail.com>
2011-02-14 19:35:30 +0200

    Add failing test for timelineRevised initialisation.

--- public/jasmine/spec/DocumentStateSpec.coffee
+++ public/jasmine/spec/DocumentStateSpec.coffee
@@ -2,7 +2,13 @@ describe "DocumentState", ->
   state = new DocumentState
     timelineOriginal:
       points: 's1,s2,s3,s4'
+    timelineRevised:
+      points: 's1,s2,s5,s6'
 
   describe "timelineOriginal", ->
   it "returns a list", ->
     expect(state.timelineOriginal).toEqual ['s1','s2','s3','s4']
+
+  describe "timelineRevised", ->
+  it "returns a list", ->
+    expect(state.timelineRevised).toEqual ['s1','s2','s5','s6']
--- public/jasmine/spec/DocumentStateSpec.js
+++ public/jasmine/spec/DocumentStateSpec.js
@@ -4,11 +4,18 @@
     state = new DocumentState({
       timelineOriginal: {
         points: 's1,s2,s3,s4'
+      },
+      timelineRevised: {
+        points: 's1,s2,s5,s6'
       }
     });
     describe("timelineOriginal", function() {});
-    return it("returns a list", function() {
+    it("returns a list", function() {
       return expect(state.timelineOriginal).toEqual(['s1', 's2', 's3', 's4']);
+    });
+    describe("timelineRevised", function() {});
+    return it("returns a list", function() {
+      return expect(state.timelineRevised).toEqual(['s1', 's2', 's5', 's6']);
     });
   });
 }).call(this);
