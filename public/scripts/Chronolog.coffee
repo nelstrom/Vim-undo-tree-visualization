01cd52c7659487deefb5f93f23d7549c5c2fa78d
Drew Neil <andrew.jr.neil@gmail.com>
2011-02-20 18:21:56 +0200

    Make Chronolog class track instances.

--- public/scripts/Chronolog.coffee
+++ public/scripts/Chronolog.coffee
@@ -1,5 +1,8 @@
 class Chronolog
   constructor: (config) ->
     @coordinates = config.coordinates
+    Chronolog.instances.push(this)
+
+  @instances: []
 
 window.Chronolog = Chronolog
--- public/scripts/Chronolog.js
+++ public/scripts/Chronolog.js
@@ -3,7 +3,9 @@
   Chronolog = (function() {
     function Chronolog(config) {
       this.coordinates = config.coordinates;
+      Chronolog.instances.push(this);
     }
+    Chronolog.instances = [];
     return Chronolog;
   })();
   window.Chronolog = Chronolog;
